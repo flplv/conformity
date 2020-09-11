6895c21a1748edd31598a64a01a6ccf611fa604b
Bug <jownasbugado@gmail.com>
2020-09-11 16:03:43 -0300

    Added project files for Unity simulator (#5)
    * folder structure
    
    * Add Unity project simulator files
    
    * Fix scene references and code formatting review
    
    * demo folder
    
    * Added readme file
    
    * removed gitKeep
    
    Co-authored-by: Jonas Mirovski <jonasmirovski@gmail.com>

---
+++ demo/UnityProject/Assets/Scripts/PublishingSensors/CameraPublisher.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPublisher : MonoBehaviour {
+    public ImageSynthesis imageSynthesis;
+	public NetMQPublisher netMQPublisher;
+	private float _timeStart;
+    private const float _sampleRate = 1f; // in seconds
+
+    private void Start() {
+		_timeStart = Time.time;
+    }
+
+    private void FixedUpdate() {
+		float elapsedTime = Time.time - _timeStart;
+		if (elapsedTime > _sampleRate) {
+			_timeStart = Time.time;
+			SendCameraFrame("sensor_camera", "_id");
+        }
+    }
+
+    private void SendCameraFrame(string frame_name, string capturePassName, int width = -1, int height = -1) {
+		if (width <= 0 || height <= 0) {
+			width = Screen.width;
+			height = Screen.height;
+		}
+
+		// execute as coroutine to wait for the EndOfFrame before capture
+		StartCoroutine(WaitForEndOfFrameAndSend(frame_name, capturePassName, width, height));
+	}
+
+	private IEnumerator WaitForEndOfFrameAndSend(string frame_name, string capturePassName, int width, int height) {
+		yield return new WaitForEndOfFrame();
+		var buffer = imageSynthesis.GetPNGBufferFromCapturePass(capturePassName, width, height);
+		netMQPublisher.PublishData(frame_name, buffer);
+	}
+}
